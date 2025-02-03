708094d4004ebb8ed78b2fb22c64fe7a6c48b837
Ismaelz001 <rodriguezortegaismael@gmial.com>
2025-02-03 06:57:40 +0100

    Cambio del server y gitignore

--- server.js
+++ server.js
@@ -1,2 +1,15 @@
-"user strict";
-console.log("Hello World");
+import express from 'express';
+
+const app = express();
+
+const PORT = 3000; // cuidao con numero de puerto
+//por si otro compañero tiene pillado este mismo numero 
+//al trabajar en github, 
+
+app.get("/", function(req, res) {
+    res.send("Hello World");
+});
+
+app.listen(PORT, function() {
+    console.log(`Server is running on PORT ${PORT}`);
+});
